ef397d9329f93ab9655ed0724b0831c5c2d70936
David Edson <david.contato.tec@gmail.com>
2021-05-02 00:19:04 -0300

    FEAT: master - create methods DELETE and SEARCH categories

--- models/category.js
+++ models/category.js
@@ -15,6 +15,21 @@ class Category
             return error? res.status(400).json(error) : res.status(201).json({ success: true, message: `Categoria ${category.name} criada com sucesso.` })
         })
     }
+
+    delete(id, res){
+        connect.query(`DELETE FROM category WHERE id = ?`, id, (error, result) => {
+            return error? res.status(400).json(error) : res.status(200).json({ success: true, message: `Categoria deletada com sucesso.` })
+        })
+    }
+
+    search(name, res){
+        
+        const formattedNameForSearch = `%${name}%`
+
+        connect.query(`SELECT * FROM category WHERE name LIKE ?`, formattedNameForSearch, (error, result) => {
+            return error? res.status(400).json(error) : res.status(200).json({ success: true, categories: result })
+        })
+    }
 }
 
 module.exports = new Category
